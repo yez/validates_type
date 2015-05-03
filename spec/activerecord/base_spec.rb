c583a3913433b7f4d7e41a6f2cccac5d98d93722
Jake Yesbeck <jake@apartmentlist.com>
2015-05-03 16:09:21 -0700

    Break out ActiveRecord tests into logical chunks
    Make sure to test the modifier functionality built into standard
    ActiveRecord validators.
    
    Specs that determine the validator adds the correct validation objects
    to the model.

---
+++ spec/activerecord/base_spec.rb
@@ -0,0 +1,33 @@
+require_relative '../spec_helper'
+
+describe 'ValidatesType' do
+  context 'validates_type :attribute' do
+    subject { TypeValidationTest.set_accessor_and_long_validator(:string) }
+
+    it 'adds a validator to the subject' do
+      klass = subject.class
+      expect(klass.validators).to_not be_empty
+      expect(klass.validators).to include(ActiveModel::Validations::TypeValidator)
+    end
+
+    it 'adds the correct validator to the subject' do
+      validator = subject.class.validators.find { |v| v.is_a?(ActiveModel::Validations::TypeValidator) }
+      expect(validator.options[:type]).to eq(:string)
+    end
+  end
+
+  context 'validates :attribute, type: { type: type }.merge(other_options)' do
+    subject { TypeValidationTest.set_accessor_and_validator(:string) }
+
+    it 'adds a validator to the subject' do
+      klass = subject.class
+      expect(klass.validators).to_not be_empty
+      expect(klass.validators).to include(ActiveModel::Validations::TypeValidator)
+    end
+
+    it 'adds the correct validator to the subject' do
+      validator = subject.class.validators.find { |v| v.is_a?(ActiveModel::Validations::TypeValidator) }
+      expect(validator.options[:type]).to eq(:string)
+    end
+  end
+end
